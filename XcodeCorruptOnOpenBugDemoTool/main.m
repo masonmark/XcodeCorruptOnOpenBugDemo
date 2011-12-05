e89976c02794a22b987a0abed6b7d7b403346efc
Mason Mark <mason@fivespeed.com>
2011-12-05 13:13:19 +0900

    Initial project as created by Xcode

---
+++ XcodeCorruptOnOpenBugDemoTool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  XcodeCorruptOnOpenBugDemoTool
+//
+//  Created by Mason Mark on 12/5/11.
+//  Copyright (c) 2011 Nakahara Informatics, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
